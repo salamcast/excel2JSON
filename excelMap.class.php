8f4f2cc92a9e44e1db86f9518cc638864b8b70d0
Karl Holz <salamcast@gmail.com>
2016-07-06 15:48:47 -0400

    Major Update
    added cell mapping to names; index.php will display the excel files
    sheets and data

--- excelMap.class.php
+++ excelMap.class.php
@@ -36,7 +36,7 @@ class excel2JSON  {
 	 * excel shared Strings data
 	 * @var string $excel_strings
 	 */
-	private $excel_strings;
+	private $excel_strings=FALSE;
 	
 	/**
 	 * loaded excel file list
@@ -50,6 +50,7 @@ class excel2JSON  {
 	 * @var string $loaded_workbook
 	 */
 	private $loaded_workbook=FALSE;
+
 	
 	/**
 	 * Loaded Workbook data
@@ -58,12 +59,25 @@ class excel2JSON  {
 	 */
 	private $loaded_workbook_data=array();
 	
+	function get_loaded_workbook_data() { return $this->loaded_workbook_data ;}
+
+	/**
+	 * Loaded Workbook data
+	 *
+	 * @var array $new_loaded_workbook_data
+	 */
+	private $new_loaded_workbook_data=array();
+
+	function get_new_loaded_workbook_data() { return $this->new_loaded_workbook_data ;}
+	
 	/**
 	 * loaded Workbook data
 	 * 
 	 * @var array $loaded_workbook_cells
 	 */
 	private $loaded_workbook_cells=array();
+	
+	function get_loaded_workbook_cells() { return $this->loaded_workbook_cells ;}
 
 	/**
 	 * loaded Workbook data
@@ -72,30 +86,80 @@ class excel2JSON  {
 	 */
 	private $loaded_workbook_rows=array();
 	
+	function get_loaded_workbook_rows () { return $this->loaded_workbook_rows ;}
+
+	/**
+	 * loaded Workbook data
+	 *
+	 * @var array $loaded_workbook_cols
+	 */
+	private $loaded_workbook_cols=array();
+
+	function get_loaded_workbook_cols () { return $this->loaded_workbook_cols ;}
+	
 	/**
 	 * Excel cell list
 	 * @var array $filter
 	 */
 	private $filter=array();
-	
-	
+
+
 	/**
 	 * load the excel file if it's past as an argument
-	 * 
+	 *
 	 * @param string $file
-	 * @return boolean
 	 */
 	function __construct($file='') {
-		if ($file != '') $this->set_excel($file);
+		if ($file != '')
+			$this->set_excel($file);
 		return TRUE;
 	}
-	
+
+	private $ini=FALSE;
+
+	function load_config() {
+		$ini=basename($this->excel, '.xlsx') . '.ini';
+		if (!is_file($ini)) {
+			foreach ($this->get_sheets() as $p){
+				$this->load_sheet($p);
+				$this->load_sheet_data();
+				$excel=$this->get_loaded_workbook_cells();
+				$ini.=<<<i
+[$p]
+i
+				;
+				foreach ($excel as $k =>$v)	$ini.="$k=\"$k\"\n";
+			}
+			/**
+			 * write configuration file as test.ini
+			 */
+			file_put_contents(basename($excel, '.xlsx') . '.ini', $ini);
+		}
+		$this->ini=parse_ini_file($ini, TRUE);
+		return TRUE;
+	}
+
+	function get_config() {
+		if (array_key_exists($this->sheet, $this->ini)) return $this->ini[$this->sheet];
+		return FALSE;
+	}
+
+	function get_sheets() {
+		$path=array();
+		/**
+		 * get a list of excel sheets and add them to $path
+		 */
+		if (!$this->file) return $path;
+		foreach ($this->excel_sheets['title'] as $k => $v) $path[]=$v;
+		return $path;
+	}
+
 	/**
 	 * Set Excel doc
 	 * Sets the excel file to use as a DB
 	 * file should have an xlsx or xlsm
-	 * @param file $file
-	 * @param string it's default is true, set to false if your sheet fails to load
+	 * @param string $file
+	 * @param string $t it's default is true, set to false if your sheet fails to load
 	 * @return boolean
 	 */
 	function set_excel($file, $t='true') { 
@@ -106,19 +170,23 @@ class excel2JSON  {
 			// load work book list
 			$sheets=trim(file_get_contents($this->file.'#'.$this->workbooks));
 			$wb=$this->xsl_out($this->GetWorkbooksList_xslt($t), $sheets);
-			if ($wb != '') $this->excel_sheets=parse_ini_string($wb); 
+			if ($wb != '') $this->excel_sheets=parse_ini_string($wb);
+			$this->load_config();
 			return TRUE;
 		} 
 		return FALSE; 
 		
 	}
-	
+
+	private $sheet = FALSE;
+
 	/**
 	 * Load Excel sheet by name
 	 * @param string $sheet
 	 * @return boolean
 	 */
 	function load_sheet($sheet) {
+		$this->sheet=$sheet;
 		if (in_array($sheet, $this->excel_sheets['title'])) {
 			$key=array_search($sheet, $this->excel_sheets['title']);
 			$this->loaded_workbook=ltrim($this->excel_sheets['xml'][$key], '/');
@@ -126,67 +194,178 @@ class excel2JSON  {
 		}
 		return FALSE;
 	}
-	
+
+
+
 	/**
 	 * Load Excel sheet data
 	 * @return boolean
 	 */
 	function load_sheet_data(){
 		if (!$this->loaded_workbook) return FALSE;
-		$data=trim(file_get_contents($this->file.'#'.$this->loaded_workbook));
-		$excel_strings=trim(file_get_contents($this->file.'#'.$this->strings));
-		$ini=$this->xsl_out($this->cellList_xslt(), $data);
+
+		$ini=$this->xsl_out($this->cellList_xslt(), $this->get_workbook());
 		if ($ini != '') {
 			$this->loaded_workbook_data=parse_ini_string($ini, TRUE);
-			foreach($this->loaded_workbook_data as $k => $v) {
-				if ($k != 'sheet') {
-					if (array_key_exists('val', $v)) {
-						if (array_key_exists('t', $v) && $v['t'] == 's') {	
-							$val=$v['val']+1;
-							$s=$this->xsl_out($this->StringLookup_xslt( $val), $excel_strings);
-							$this->loaded_workbook_data[$k]['val']=$s;
-							$this->loaded_workbook_cells[$k]=$s;
-							$this->loaded_workbook_rows[$v['row']][$k]=$s;
-							// drop the 't' key off of the array, it's only needed for text look up
-							array_pop($this->loaded_workbook_data[$k]);
-					 	}  else {
-					 		$this->loaded_workbook_cells[$k]=$v['val'];
-					 		$this->loaded_workbook_rows[$v['row']][$k]=$v['val'];
-						}
-					}
+			if(array_key_exists('sheet', $this->loaded_workbook_data)) {
+				if(array_key_exists('dimension', $this->loaded_workbook_data['sheet'])) {
+					$this->col_start = $this->get_col($this->loaded_workbook_data['sheet']['dimension'][0]);
+					$this->col_end   = $this->get_col($this->loaded_workbook_data['sheet']['dimension'][1]);
+					$this->row_start = $this->get_row($this->loaded_workbook_data['sheet']['dimension'][0]);
+					$this->row_end   = $this->get_row($this->loaded_workbook_data['sheet']['dimension'][1]);
 				}
 			}
+			array_shift($this->loaded_workbook_data);
+			//setup excel cell filter
+
+
+
+			foreach($this->loaded_workbook_data as $k => $v) {
+				if (array_key_exists($k, $this->ini[$this->sheet])) {
+					$label=$this->ini[$this->sheet][$k];
+				} else {
+					$label=FALSE;
+				}
+
+				if (array_key_exists('val', $v)) {
+					if (array_key_exists('t', $v) && $v['t'] == 's') {
+						$val=$v['val']+1; //add 1 so search matches value in excel_strings
+						$s=$this->xsl_out($this->StringLookup_xslt( $val), $this->get_excel_strings());
+
+						if ($label) $this->new_loaded_workbook_data[$label]['val']=$s;
+
+						$this->loaded_workbook_data[$k]['val']=$s;
+
+						$this->loaded_workbook_cells[$k]=$s;
+
+						$this->loaded_workbook_rows[$this->get_row($k)][$this->get_col($k)]=$s;
+
+						$this->loaded_workbook_cols[$this->get_col($k)][$this->get_row($k)]=$s;
+
+						// drop the 't' key off of the array, it's only needed for text look up
+						array_pop($this->loaded_workbook_data[$k]);
+					}  else {
+
+						if ($label) $this->loaded_workbook_cells[$label]=$v['val'];
+
+						$this->loaded_workbook_rows[$this->get_row($k)][$this->get_col($k)]=$v['val'];
+
+						$this->loaded_workbook_cols[$this->get_col($k)][$this->get_row($k)]=$v['val'];
+					}
+
+
+
+					// new workbook data with custom label
+					if ($label){
+						$this->new_loaded_workbook_data[$label]['col']=$this->get_col($k);
+						$this->new_loaded_workbook_data[$label]['row']=$this->get_row($k);
+						$this->new_loaded_workbook_data[$label]['cell']=$k;
+
+					}
+
+
+					//updated workbook data
+					$this->loaded_workbook_data[$k]['col']=$this->get_col($k);
+					$this->loaded_workbook_data[$k]['row']=$this->get_row($k);
+					$this->loaded_workbook_data[$k]['cell']=$k;
+					if ($label) $this->loaded_workbook_data[$k]['label']=$label;
+
+				}
+
+			}
+
+			foreach ($this->ini[$this->sheet] as $k => $v) {
+				$this->add_cell_list($k);
+			}
+
+			//	echo '<pre>';print_r($this->ini[$this->sheet]); echo '</pre>'; exit();
+
 			return TRUE;
 		}
 		return FALSE;
 	}
-	
+
+	private $col_start = 'A';
+
+	private $col_end = 'ZZ';
+
+	private $row_start = 1;
+
+	private $row_end = 512;
+
+
+	/**
+	 * get the loaded workbook page
+	 * @return string
+	 */
+	function get_workbook() {
+		return trim(file_get_contents($this->file.'#'.$this->loaded_workbook));
+	}
+
+	/**
+	 * get Excel strings from the selected file
+	 *
+	 * @return string
+	 */
+	function get_excel_strings() {
+		return trim(file_get_contents($this->file.'#'.$this->strings));
+	}
+
+	/**
+	 * returns the colum used in excel
+	 *
+	 * example A2 will return A
+	 *
+	 * @param $cell
+	 * @return string
+	 */
+	function get_col($cell) {
+		return preg_replace('/\d*/', '', $cell);
+	}
+
+	/**
+	 * returns the row used in excel
+	 *
+	 * example B6 will return 6
+	 *
+	 * @param $cell
+	 * @return int
+	 */
+	function get_row($cell) {
+		return preg_replace('/[A-Za-z]*/', '', $cell);
+	}
 
 	/**
 	 * Cell List
 	 * add an excel cell reference to filter list
 	 * @param string $cell
+	 * @return bool
 	 */
 	function add_cell_list($cell) {
 		if (array_key_exists($cell, $this->loaded_workbook_cells)) $this->filter[]=$cell;
+		return TRUE;
 	}
 	
 	/**
 	 * Print Rows
 	 * print sheet cell rows as JSON Document, it will basicly be { "excel cell":"excel data" }
+	 * @return bool
 	 */
 	function print_sheet_rows() {
 		if (!$this->loaded_workbook) $this->print_sheets();
 		$this->json_out($this->loaded_workbook_rows);
+		return TRUE;
 	}
 	
 	/**
 	 * Print Sheet
 	 * print sheet data as JSON Document, it will basicly be { "excel cell" : { "row": "row number", "val": "excel data" } }
+	 * @return bool
 	 */
 	function print_sheet_data() { 
 		if (!$this->loaded_workbook) $this->print_sheets();
-		$this->json_out($this->loaded_workbook_data); 
+		$this->json_out($this->loaded_workbook_data);
+		return TRUE;
 	}
 	
 	/**
@@ -196,7 +375,7 @@ class excel2JSON  {
 	 */
 	function filter_data() {
 		$filter=array();
-		foreach ($this->filter as $f) $filter[$f]=$this->loaded_workbook_data[$f];
+		foreach ($this->filter as $f) $filter[]=$this->loaded_workbook_data[$f];
 		return $filter;
 	}
 	
@@ -229,7 +408,9 @@ class excel2JSON  {
 	 */
 	function filter_cells() {
 		$filter=array();
-		foreach ($this->filter as $f) $filter[$f]=$this->loaded_workbook_cells[$f];
+		foreach ($this->filter as $f)
+			if (array_key_exists($f, $this->ini[$this->sheet]))
+				$filter[$this->ini[$this->sheet][$f]]=$this->loaded_workbook_cells[$f];
 		return $filter;
 	}
 	
@@ -237,10 +418,12 @@ class excel2JSON  {
 	 * Print filtered cells
 	 * Print filtered sheet cells as JSON Document, it will basicly be { "excel cell":"excel data" }
 	 *
+	 * @return bool
 	 */
 	function print_filter_cells() {
 		if (!$this->loaded_workbook) $this->print_sheets();
 		$this->json_out($this->filter_cells());
+		return TRUE;
 	}
 	
 	/**
@@ -248,8 +431,13 @@ class excel2JSON  {
 	 * print workbook sheet names as JSON Document
 	 *
 	 * it will basicly be [ "sheet name" ]
+	 *
+	 * @return bool
 	 */
-	function print_sheets() { $this->json_out($this->excel_sheets['title']); }
+	function print_sheets() {
+		$this->json_out($this->excel_sheets['title']);
+		return TRUE;
+	}
 	
 	/**
 	 * Open Zip
@@ -335,7 +523,7 @@ dimension[]="<xsl:value-of select="substring-after(//dimension/@ref|//w:dimensio
  <xsl:template match="//c|//w:c">
   <xsl:if test="v|w:v">
 [<xsl:value-of select="@r"/>]
-row="<xsl:value-of select="../@r"/>"
+; row="<xsl:value-of select="../@r"/>"
 val="<xsl:value-of select="v|w:v"/>"
 <xsl:if test="@t">t="<xsl:value-of select="@t"/>"</xsl:if>
   </xsl:if>
